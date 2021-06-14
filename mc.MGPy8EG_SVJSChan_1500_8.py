8e4de58c765d5fc3f095dbf263c92ef263dfaa80
Bingxuan Liu <bingxuan.liu@cern.ch>
2021-06-14 17:06:21 +0200

    Update

--- mc.MGPy8EG_SVJSChan_1500_8.py
+++ mc.MGPy8EG_SVJSChan_1500_8.py
@@ -4,10 +4,10 @@ import re
 import math, cmath
 import os
 
-nevents=runArgs.maxEvents
+nevents = runArgs.maxEvents*6.5 if runArgs.maxEvents>0 else 6.5*evgenConfig.nEventsPerJob
 
 bonus_file = open('pdg_extras.dat','w')
-#s-channel specific  
+#  The most important number is the first: the PDGID of the particle
 bonus_file.write('9000002\n')
 bonus_file.write('9000003\n')
 bonus_file.write('9000004\n')
@@ -24,7 +24,7 @@ print("Mz ", Mz)
 print("Rinv ", Rinv)
 
 masses={
-    '1000022': str(10),
+    '5000521': str(10),
     '5000001': str(Mz)
 }
 
@@ -37,6 +37,9 @@ dminputs={
     '8': str(0.1),
     '9': str(0.1),
     '25': str(0),
+    '26': str(0),
+    '27': str(0),
+    '28': str(0)
 }
 
 decays={
@@ -47,31 +50,33 @@ params={'MASS':masses,
         'DMINPUTS':dminputs,
         'DECAY':decays}
 
+
 my_process = """
-import model  DMsimp_s_spin1 --modelname
+import model  DMsimp_s_spin1_SVJ
 define j = g u c d b t s u~ c~ d~ b~ s~ t~
-generate p p > xd xd~ 
-add process p p > xd xd~ j 
+generate p p > xd xd~
+add process p p > xd xd~ j
 add process p p > xd xd~ j j
 output -f
 """
 
-run_settings = {'nevents':int(nevents),
-                'lhe_version':'3.0',
-                'cut_decays':'F',
-                'ickkw'        : 0,
-                'drjj'         : 0.0,
-                'maxjetflavor' : 5,
-                'xqcut'        : 0.0,
-                'ktdurham'     : 100,
-                'dparameter'   : 0.4,
-                }
-
 process_dir = new_process(my_process)
 runName='run_01'
 
+run_settings = {'lhe_version':'2.0',
+          'cut_decays':'F',
+	  'ickkw'        : 0,
+          'drjj'         : 0.0,
+          'maxjetflavor' : 5,
+          'xqcut'        : 0.0,
+          'ktdurham'     : 100,
+          'dparameter'   : 0.4,
+          'nevents'      : int(nevents)
+          }
+
 modify_param_card(process_dir=process_dir,params=params)
 modify_run_card(process_dir=process_dir,runArgs=runArgs,settings=run_settings)
+
 generate(process_dir=process_dir,runArgs=runArgs)
 
 unzip1 = subprocess.Popen(['gunzip',process_dir+'/Events/'+runName+'/unweighted_events.lhe.gz'])
@@ -80,8 +85,8 @@ oldlhe = open(process_dir+'/Events/'+runName+'/unweighted_events.lhe','r')
 newlhe = open(process_dir+'/Events/'+runName+'/unweighted_events2.lhe','w')
 init = True
 for line in oldlhe:
-            if '1000022' in line:
-                line = line.replace('1000022','4900101')                           
+            if '5000521' in line:
+                line = line.replace('5000521','4900101')                           
             newlhe.write(line)
 oldlhe.close()
 newlhe.close()
@@ -104,36 +109,23 @@ runArgs.inputGeneratorFile = outDS
 
 #### Shower 
 evgenConfig.description = "Semivisible jets s-chan"
-evgenConfig.keywords+=['BSM','sChannel']
+evgenConfig.keywords+=['BSM',"sChannel"]
 evgenConfig.generators+=["MadGraph","Pythia8","EvtGen"]
 evgenConfig.contact  = ['bingxuan.liu@cern.ch']
-evgenConfig.process = "p p --> xd xd~ j j"
+evgenConfig.process = "p p --> xd xd~"
 
+PYTHIA8_nJetMax=2
+PYTHIA8_Process='pp>xdxd~'
+PYTHIA8_Dparameter=run_settings['dparameter']
+PYTHIA8_TMS=run_settings['ktdurham']
+PYTHIA8_nQuarksMerge=run_settings['maxjetflavor']
+
+###Pythia8 commands
 include("Pythia8_i/Pythia8_A14_NNPDF23LO_EvtGen_Common.py")
 include("Pythia8_i/Pythia8_MadGraph.py")
+include("Pythia8_i/Pythia8_CKKWL_kTMerge.py")
 
-genSeq.Pythia8.Commands += ["JetMatching:merge = on"]
-genSeq.Pythia8.Commands += ["JetMatching:scheme = 1"]
-genSeq.Pythia8.Commands += ["JetMatching:setMad = on"]
-genSeq.Pythia8.Commands += ["Check:epTolErr = 1e-2"]
-genSeq.Pythia8.Commands += ["JetMatching:nJetMax = 2"]
-genSeq.Pythia8.Commands += ["JetMatching:coneRadius = 0.4"]
-genSeq.Pythia8.Commands += ["JetMatching:etaJetMax = 10.0"]
-
-if "UserHooks" in genSeq.Pythia8.__slots__.keys():
-  genSeq.Pythia8.UserHooks += ['JetMatchingMadgraph']
-else:
-  genSeq.Pythia8.UserHook = 'JetMatchingMadgraph'
-
-genSeq.Pythia8.Commands+=["HiddenValley:Ngauge  = 2"]
-genSeq.Pythia8.Commands+=["HiddenValley:spinFv = 0"]
-genSeq.Pythia8.Commands+=["HiddenValley:FSR = on"]
-genSeq.Pythia8.Commands+=["HiddenValley:fragment = on"]
-genSeq.Pythia8.Commands+=["HiddenValley:alphaOrder = 1"]
-genSeq.Pythia8.Commands+=["HiddenValley:Lambda = 5.0"]
-genSeq.Pythia8.Commands+=["HiddenValley:pTminFSR = 1.1"]
-genSeq.Pythia8.Commands+=["HiddenValley:probVector = 0.75"]
-genSeq.Pythia8.Commands+=["HiddenValley:nFlav  = 2 "]
+genSeq.Pythia8.Commands+=["Merging:doKTMerging=on"]
 
 genSeq.Pythia8.Commands+=["4900001:m0 = 5000"]
 genSeq.Pythia8.Commands+=["4900002:m0 = 5000"]
@@ -147,17 +139,27 @@ genSeq.Pythia8.Commands+=["4900013:m0 = 5000"]
 genSeq.Pythia8.Commands+=["4900014:m0 = 5000"]
 genSeq.Pythia8.Commands+=["4900015:m0 = 5000"]
 genSeq.Pythia8.Commands+=["4900016:m0 = 5000"]
-genSeq.Pythia8.Commands+=["4900101:m0 = 10"]
+
+# Fix dark hadron mass?
+genSeq.Pythia8.Commands+=["HiddenValley:Ngauge  = 2"]
+genSeq.Pythia8.Commands+=["HiddenValley:alphaOrder = 1"]
+genSeq.Pythia8.Commands+=["HiddenValley:Lambda = 5.0"]
+genSeq.Pythia8.Commands+=["HiddenValley:nFlav  = 2 "]
+genSeq.Pythia8.Commands+=["HiddenValley:spinFv = 0"]
+genSeq.Pythia8.Commands+=["HiddenValley:FSR = on"]
+genSeq.Pythia8.Commands+=["HiddenValley:fragment = on"]
+genSeq.Pythia8.Commands+=["HiddenValley:pTminFSR = 5.5"]
+genSeq.Pythia8.Commands+=["HiddenValley:probVector = 0.75"]
+
 genSeq.Pythia8.Commands+=["4900101:mWidth = 0.2"]
 genSeq.Pythia8.Commands+=["4900101:mMin = 9.8"]
 genSeq.Pythia8.Commands+=["4900101:mMax = 10.2"]
 genSeq.Pythia8.Commands+=["4900111:m0 = 20.0"]
 genSeq.Pythia8.Commands+=["4900113:m0 = 20.0"]
-genSeq.Pythia8.Commands+=["51:m0 = 9.99"]
-genSeq.Pythia8.Commands+=["53:m0 = 9.99"]
-#s-channel specific
 genSeq.Pythia8.Commands+=["4900211:m0 = 20.0"]
 genSeq.Pythia8.Commands+=["4900213:m0 = 20.0"]
+genSeq.Pythia8.Commands+=["51:m0 = 9.99"]
+genSeq.Pythia8.Commands+=["53:m0 = 9.99"]
 
 genSeq.Pythia8.Commands+=["4900111:onechannel = 1 {0} 91 -3 3".format(1 - Rinv)] # check Rinv syntax
 genSeq.Pythia8.Commands+=["4900111:addchannel = 1 {0} 0 51 -51".format(Rinv)]
@@ -169,13 +171,14 @@ genSeq.Pythia8.Commands+=["4900113:addchannel = 1 {0}  91 -4 4".format((1-Rinv)/
 genSeq.Pythia8.Commands+=["4900113:addchannel = 1 {0}  91 -5 5".format((1-Rinv)/5)]
 genSeq.Pythia8.Commands+=["4900113:addchannel = 1 {0} 0 53 -53".format(Rinv)] 
 
-#s-channel specific
 genSeq.Pythia8.Commands+=["4900211:onechannel = 1 {0} 91 -3 3".format(1 - Rinv)] # check Rinv syntax
 genSeq.Pythia8.Commands+=["4900211:addchannel = 1 {0} 0 51 -51".format(Rinv)]
+
 genSeq.Pythia8.Commands+=["4900213:onechannel = 1 {0}  91 -1 1".format((1-Rinv)/5)] 
 genSeq.Pythia8.Commands+=["4900213:addchannel = 1 {0}  91 -2 2".format((1-Rinv)/5)]
 genSeq.Pythia8.Commands+=["4900213:addchannel = 1 {0}  91 -3 3".format((1-Rinv)/5)]
 genSeq.Pythia8.Commands+=["4900213:addchannel = 1 {0}  91 -4 4".format((1-Rinv)/5)]
 genSeq.Pythia8.Commands+=["4900213:addchannel = 1 {0}  91 -5 5".format((1-Rinv)/5)]
-genSeq.Pythia8.Commands+=["4900213:addchannel = 1 {0} 0 -53 -53".format(Rinv)] 
+genSeq.Pythia8.Commands+=["4900213:addchannel = 1 {0} 0 53 -53".format(Rinv)] 
 
+genSeq.Pythia8.Commands+=["Merging:mayRemoveDecayProducts=on"] 
